cf3996ace4f4c0fe7119f55da1cd7f95f4e279f0
Jonathan Querubina <jonathan@d3.do>
2016-01-14 18:02:09 -0200

    get now playing with artist info and song preview link from spotify

--- last.fm.js
+++ last.fm.js
@@ -1,4 +1,6 @@
 var LastFmNode = require('lastfm').LastFmNode;
+var http = require('http');
+var https = require('https');
 
 var lastfm = new LastFmNode({
   api_key: '7d0a3a11116a3f166a5b71674e825355',
@@ -12,7 +14,52 @@ trackStream.on('lastPlayed', function(track) {
 });
 
 trackStream.on('nowPlaying', function(track) {
-  console.log('Now playing: ', track.name, ' - ' + track.artist["#text"]);
+
+  http.get({
+    hostname: 'developer.echonest.com',
+    path: '/api/v4/artist/terms?api_key=DJQBV7G7ZFUC7CZAZ&format=json&name='+track.artist["#text"],
+  }, (response) => {
+
+    var data = "";
+    response.on("data", function (chunk) {
+        data += chunk;
+    });
+    response.on("end", function () {
+        var genres = [];
+        var terms = JSON.parse(data).response.terms;
+        terms.forEach(function(item){
+          genres.push(item.name);
+        });
+
+        data = '';
+
+        https.get({
+          hostname: 'api.spotify.com',
+          path: '/v1/search?query='+encodeURIComponent(track.name)+'&offset=0&limit=50&type=track'
+        }, (response) => {
+          response.on("data", function(chunk){
+            data += chunk;
+          });
+          response.on("end", function(){
+            var tracks = JSON.parse(data).tracks.items;
+            var preview_url = 0;
+            tracks.forEach(function(item){
+              var artists = item.artists;
+              artists.forEach(function(artist){
+                if ( artist.name === track.artist["#text"] ){
+                  preview_url = item.preview_url
+                }
+              });
+            });
+            console.log(preview_url);
+          });
+        });
+        //console.log('Now playing: ', track.name +' - '+ track.artist["#text"]);
+        console.log('Now playing: \nMusic:', track.name, '\nArist: ' + track.artist["#text"], ' \nGenres: ('+genres.toString()+')');
+    });
+  });
+
+
 });
 
 trackStream.on('scrobbled', function(track) {

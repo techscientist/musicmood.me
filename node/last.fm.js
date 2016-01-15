08850e6412739e5f7c081233dedfd61879dca9d1
Jonathan Querubina <jonathan@d3.do>
2016-01-15 12:12:21 -0200

    minor updates

--- node/last.fm.js
+++ node/last.fm.js
@@ -13,81 +13,85 @@ var user = 'chr0nu5';
 var trackStream = lastfm.stream(user);
 
 function processTrack(track){
-  http.get({
-    hostname: 'developer.echonest.com',
-    path: '/api/v4/artist/terms?api_key=DJQBV7G7ZFUC7CZAZ&format=json&name='+encodeURIComponent(track.artist["#text"]),
-  }, (response) => {
+  if (track){
+    http.get({
+      hostname: 'developer.echonest.com',
+      path: '/api/v4/artist/terms?api_key=DJQBV7G7ZFUC7CZAZ&format=json&name='+encodeURIComponent(track.artist["#text"]),
+    }, (response) => {
 
-    var data = "";
-    response.on("data", function (chunk) {
-        data += chunk;
-    });
-    response.on("end", function () {
-        var genres = [];
-        var terms = JSON.parse(data).response.terms;
-        terms.forEach(function(item){
-          genres.push(item.name);
-        });
-
-        data = '';
-
-        https.get({
-          hostname: 'api.spotify.com',
-          path: '/v1/search?query='+encodeURIComponent(track.name)+'&offset=0&limit=50&type=track'
-        }, (response) => {
-          response.on("data", function(chunk){
-            data += chunk;
+      var data = "";
+      response.on("data", function (chunk) {
+          data += chunk;
+      });
+      response.on("end", function () {
+          var genres = [];
+          var terms = JSON.parse(data).response.terms;
+          terms.forEach(function(item){
+            genres.push(item.name);
           });
-          response.on("end", function(){
-            var tracks = JSON.parse(data).tracks.items;
-            var preview_url = undefined;
-            tracks.forEach(function(item){
-              var artists = item.artists;
-              artists.forEach(function(artist){
-                if ( artist.name === track.artist["#text"] ){
-                  preview_url = item.preview_url
-                }
-              });
+
+          data = '';
+
+          https.get({
+            hostname: 'api.spotify.com',
+            path: '/v1/search?query='+encodeURIComponent(track.name)+'&offset=0&limit=50&type=track'
+          }, (response) => {
+            response.on("data", function(chunk){
+              data += chunk;
             });
-
-            if ( preview_url ){
-              var dir = 'mkdir -p ../tmp';
-              var child = exec(dir, function(err, stdout, stderr) {
-                  if (err) {
-                    throw err;
-                  }else{
-                    var file = fs.createWriteStream('../tmp/' + user + '.mp3');
-                    https.get({
-                      hostname: 'p.scdn.co',
-                      path: preview_url.replace('https://p.scdn.co','')
-                    }, (response) => {
-                      response.on("data",function(data){
-                        file.write(data);
-                      });
-                      response.on("end",function(){
-                        file.end();
-
-                        var processFile = exec('python ../python/tempo-beats.py ../tmp/' + user + '.mp3', function(err, stdout, stderr) {
-                          if (err) {
-                            throw err;
-                          }else{
-                            var bpm = stdout.replace('\n','');
-                            console.log('\nMusic:', track.name, '\nArist: ' + track.artist["#text"], ' \nGenres: ('+genres.toString()+') \nBPM: '+bpm);
-                          }
-                        });
-
-                      })
-                    })
+            response.on("end", function(){
+              var tracks = JSON.parse(data).tracks.items;
+              var preview_url = undefined;
+              tracks.forEach(function(item){
+                var artists = item.artists;
+                artists.forEach(function(artist){
+                  if ( artist.name === track.artist["#text"] ){
+                    preview_url = item.preview_url
                   }
+                });
               });
 
-            }else{
-              console.log('\nfile not found for processing');
-            }
+              if ( preview_url ){
+                var dir = 'mkdir -p ../tmp';
+                var child = exec(dir, function(err, stdout, stderr) {
+                    if (err) {
+                      throw err;
+                    }else{
+                      var file = fs.createWriteStream('../tmp/' + user + '.mp3');
+                      https.get({
+                        hostname: 'p.scdn.co',
+                        path: preview_url.replace('https://p.scdn.co','')
+                      }, (response) => {
+                        response.on("data",function(data){
+                          file.write(data);
+                        });
+                        response.on("end",function(){
+                          file.end();
+
+                          var processFile = exec('python ../python/tempo-beats.py ../tmp/' + user + '.mp3', function(err, stdout, stderr) {
+                            if (err) {
+                              throw err;
+                            }else{
+                              var bpm = stdout.replace('\n','');
+                              console.log('\nMusic:', track.name, '\nArist: ' + track.artist["#text"], ' \nGenres: ('+genres.toString()+') \nBPM: '+bpm);
+                            }
+                          });
+
+                        })
+                      })
+                    }
+                });
+
+              }else{
+                console.log('\nfile not found for processing');
+              }
+            });
           });
-        });
+      });
     });
-  });
+  }else{
+    console.log('There`s no music');
+  }
 }
 
 trackStream.on('lastPlayed', function(track) {

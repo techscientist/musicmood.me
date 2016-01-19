cf21424467e10c97ffa997106c0b2204d3398346
Jonathan Querubina <jonathan@d3.do>
2016-01-19 20:15:39 -0200

    first let integration, works!

--- node/last.fm.js
+++ node/last.fm.js
@@ -1,77 +1,73 @@
-// var LastFmNode = require('lastfm').LastFmNode;
-// var tools = require('./lib/tools');
-//
-// var lastfm = new LastFmNode({
-//     api_key: tools.LASTFM_API_KEY,
-//     secret: tools.LASTFM_API_SEC
-// });
-//
-// var trackStream = lastfm.stream('chr0nu5');
-//
-// function processTrack(track) {
-//     tools.processTrack(track)
-//         .then((info) => {
-//             console.log(`\nMusic: ${info.music}\nArtist: ${info.artist}\nGenres: (${info.genres.toString()}) \nBPM: ${info.bpm}`);
-//             console.log(`HarPer length: ${info.harper.length}`);
-//             console.log(`Duration (MS): ${info.duration}`);
-//         })
-//         .catch((err) => {
-//             console.error(err);
-//         });
-// }
-//
-// trackStream.on('nowPlaying', processTrack)
-//     .on('error', function(error) {
-//         console.log('Weird Error: ', error);
-//     });
-//
-// trackStream.start();
-
-var serialPort = require("serialport");
-serialPort.list(function(err, ports) {
-    ports.forEach(function(port) {
-        console.log(port.comName);
-        console.log(port.pnpId);
-        console.log(port.manufacturer);
-    });
-});
+var LastFmNode = require('lastfm').LastFmNode;
+var tools = require('./lib/tools');
 
 var SerialPort = require("serialport").SerialPort
-var serialPort = new SerialPort("/dev/ttyACM0", {
-    baudrate: 230400
+var serialPort = new SerialPort("/dev/cu.usbmodem1412", {
+    baudrate: 921600
 });
 
-function chr(codePt) {
-    return String.fromCharCode(codePt);
+var lastfm = new LastFmNode({
+    api_key: tools.LASTFM_API_KEY,
+    secret: tools.LASTFM_API_SEC
+});
+
+var trackStream = lastfm.stream('chr0nu5'),
+    transfer = false;
+var harper, total, duration;
+
+function createBuffer(list) {
+    var buffer = new Buffer(list.length);
+    list.forEach((item, index) => {
+        buffer[index] = item
+    });
+    return buffer;
 }
 
-serialPort.on("open", function() {
-    console.log('open');
-    serialPort.on('data', function(data) {
-        console.log('data received: ' + data);
+function writeBuffer(buffer) {
+    serialPort.open(function(error) {
+        if (error) {
+            console.log('failed to open: ' + error);
+        } else {
+            serialPort.write(buffer, (err, results) => {
+                //console.log('err ' + err);
+                //console.log('results ' + results);
+            });
+        }
     });
-    serialPort.write(chr(0x6B), function(err, results) {
-        console.log('err ' + err);
-        console.log('results ' + results);
+}
+
+function equalizer() {
+    if (transfer && harper.length > 0) {
+        var eq = harper[0],
+            percent = (30 * eq / 1000).toFixed(0);
+        harper.shift();
+        writeBuffer(createBuffer([0x6B, 0x8D, 255, 0, 0, percent]));
+        //console.log(percent, eq, harper.length);
+        setTimeout(equalizer, 200);
+    }
+}
+
+function processTrack(track) {
+    tools.processTrack(track)
+        .then((info) => {
+            console.log(`\nMusic: ${info.music}\nArtist: ${info.artist}\nGenres: (${info.genres.toString()}) \nBPM: ${info.bpm}`);
+            harper = info.harper;
+            total = harper.length;
+            duration = info.duration;
+            writeBuffer(createBuffer([0x6B, 0x8D, 255, 0, 0, 5]));
+            transfer = true;
+            equalizer();
+        })
+        .catch((err) => {
+            transfer = false;
+            console.error(err);
+        });
+}
+
+trackStream.on('nowPlaying', processTrack)
+    .on('error', function(error) {
+        transfer = false;
+        console.log('Weird Error: ', error);
     });
-    serialPort.write(chr(0x8D), function(err, results) {
-        console.log('err ' + err);
-        console.log('results ' + results);
-    });
-    serialPort.write(chr(255), function(err, results) {
-        console.log('err ' + err);
-        console.log('results ' + results);
-    });
-    serialPort.write(chr(0), function(err, results) {
-        console.log('err ' + err);
-        console.log('results ' + results);
-    });
-    serialPort.write(chr(255), function(err, results) {
-        console.log('err ' + err);
-        console.log('results ' + results);
-    });
-    serialPort.write(chr(2), function(err, results) {
-        console.log('err ' + err);
-        console.log('results ' + results);
-    });
-});
+
+trackStream.start();

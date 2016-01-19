9a348f1994cd8979635d3af114647c4554d5828c
Jonathan Querubina <jonathan@d3.do>
2016-01-19 11:30:32 -0200

    serial port initial

--- node/last.fm.js
+++ node/last.fm.js
@@ -35,3 +35,19 @@ serialPort.list(function (err, ports) {
     console.log(port.manufacturer);
   });
 });
+
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/ttyAMA0", {
+  baudrate: 230400
+});
+
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+  });
+  serialPort.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+});

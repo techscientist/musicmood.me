2db22da658f19f187a279d60b2d17e394f71a583
Jonathan Querubina <jonathan@d3.do>
2016-01-19 12:38:07 -0200

    serial port initial

--- node/last.fm.js
+++ node/last.fm.js
@@ -28,65 +28,50 @@
 // trackStream.start();
 
 var serialPort = require("serialport");
-serialPort.list(function (err, ports) {
-  ports.forEach(function(port) {
-    console.log(port.comName);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
-  });
+serialPort.list(function(err, ports) {
+    ports.forEach(function(port) {
+        console.log(port.comName);
+        console.log(port.pnpId);
+        console.log(port.manufacturer);
+    });
 });
 
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyACM0", {
-  baudrate: 230400
+    baudrate: 230400
 });
 
 function chr(codePt) {
-  //  discuss at: http://phpjs.org/functions/chr/
-  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // improved by: Brett Zamir (http://brett-zamir.me)
-  //   example 1: chr(75) === 'K';
-  //   example 1: chr(65536) === '\uD800\uDC00';
-  //   returns 1: true
-  //   returns 1: true
-
-  if (codePt > 0xFFFF) { // Create a four-byte string (length 2) since this code point is high
-    //   enough for the UTF-16 encoding (JavaScript internal use), to
-    //   require representation with two surrogates (reserved non-characters
-    //   used for building other characters; the first is "high" and the next "low")
-    codePt -= 0x10000;
-    return String.fromCharCode(0xD800 + (codePt >> 10), 0xDC00 + (codePt & 0x3FF));
-  }
-  return String.fromCharCode(codePt);
+    return String.fromCharCode(codePt);
 }
 
-serialPort.on("open", function () {
-  console.log('open');
-  serialPort.on('data', function(data) {
-    console.log('data received: ' + data);
-  });
-  serialPort.write(chr(0x6B), function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
-  serialPort.write(chr(0x8D), function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
-  serialPort.write(chr(255), function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
-  serialPort.write(chr(0), function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
-  serialPort.write(chr(255), function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
-  serialPort.write(chr(2), function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
+serialPort.on("open", function() {
+    console.log('open');
+    serialPort.on('data', function(data) {
+        console.log('data received: ' + data);
+    });
+    serialPort.write(chr(0x6B), function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
+    serialPort.write(chr(0x8D), function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
+    serialPort.write(chr(255), function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
+    serialPort.write(chr(0), function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
+    serialPort.write(chr(255), function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
+    serialPort.write(chr(2), function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+    });
 });

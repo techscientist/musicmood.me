eb4e9343b3948f6022e31a76b7cf2ad9e546df56
Jonathan Querubina <jonathan@d3.do>
2016-01-19 11:40:18 -0200

    serial port initial

--- python/serial.py
+++ python/serial.py
@@ -1,24 +1,25 @@
+# import serial
+#
+# numOfStrips = 42   #number of vertical LED strips
+#
+# serial_matrix = []
+# serial_matrix.append(0)
+# serial_matrix = serial_matrix * numOfStrips
+#
+# mSerial = None
+# try:
+# 	if inRASPBERRY:
+# 		serdev = '/dev/ttyACM0'
+# 	else:
+# 		serdev = '/dev/tty.usbserial-A400fYHu' #usb -> serial port
+#
+# 	mSerial = serial.Serial(serdev, baudrate = 230400)
+
 import serial
-
-numOfStrips = 42   #number of vertical LED strips
-
-serial_matrix = []
-serial_matrix.append(0)
-serial_matrix = serial_matrix * numOfStrips
-
-mSerial = None
-try:
-	if inRASPBERRY:
-		serdev = '/dev/ttyACM0'
-	else:
-		serdev = '/dev/tty.usbserial-A400fYHu' #usb -> serial port
-
-	mSerial = serial.Serial(serdev, baudrate = 230400)
+serial.Serial('/dev/ttyACM0', baudrate = 230400)
 
 mSerial.write(chr(0x6B))
 mSerial.write(chr(0x8D))
 mSerial.write(chr(0))
 mSerial.write(chr(0))
 mSerial.write(chr(0))
-for x in xrange(0, numOfStrips):
-	mSerial.write(chr(serial_matrix[x]))
